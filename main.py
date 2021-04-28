71ad12345c52941e29ad87065231bfb37b6cbbc6
Patrick Francey <patrickfrancey@hotmail.com>
2021-04-28 18:13:30 +0100

    Set game loop and window

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello world!")
+import pygame
+
+res = (600,400)
+fps = 60
+screen = pygame.display.set_mode(res)
+running = True 
+clock = pygame.time.Clock()
+
+while running: 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+    
+    clock.tick(fps)
