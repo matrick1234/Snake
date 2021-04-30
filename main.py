46f615421383e057f7fc70589e19c6dcc103518c
Patrick Francey <patrickfrancey@hotmail.com>
2021-04-30 16:32:48 +0100

    Added movement to square

--- main.py
+++ main.py
@@ -1,15 +1,39 @@
 import pygame
-
+pygame.init()
 res = (600,400)
 fps = 60
 screen = pygame.display.set_mode(res)
+pygame.display.set_caption("Snake")
 running = True 
 clock = pygame.time.Clock()
+
+
+snakePos = [300,200]
+
+
+
 
 while running: 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
-    
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                snakePos[1] -= 10
+            if event.key == pygame.K_DOWN:
+                snakePos[1] += 10
+            if event.key == pygame.K_RIGHT:
+                snakePos[0] += 10
+            if event.key == pygame.K_LEFT:
+                snakePos[0] -= 10
+
+    testRect = pygame.Rect(snakePos[0],snakePos[1],100,150)
+
+    screen.fill((255,0,0))
+
+    pygame.draw.rect(screen,(0,255,0),testRect)
+
+    pygame.display.update()
     clock.tick(fps)
+
