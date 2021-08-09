88fdf5ec602b7e994ba8f5e77be4834810b43474
Patrick Francey <patrickfrancey@hotmail.com>
2021-08-09 12:47:04 +0100

    ACTUALLY WORKING WITH GOOD COLLISIONS

--- Snake3.py
+++ Snake3.py
@@ -1,18 +1,17 @@
 import pygame
 import random 
 import sys
-
+import time
 pygame.init()
 
 clock = pygame.time.Clock()
-speed = 5 
-score =0 
-AppleSprite = pygame.sprite.Group()
+speed = 5
+score = 0 
 
 class Snake():
     def __init__(self):
         self.direction = "East" 
-        self.position = (300,200)
+        self.position = [300,200]
         self.head = [[100,50],[90,50],[80,50]]
         self.Turning = self.direction
 
@@ -27,7 +26,7 @@ class Snake():
             self.direction = "South"   
    
 
-    def move(self, applePos):
+    def move(self, ApplePos):
         if self.direction == "North":
             self.position[1] -= speed
         if self.direction == "South":
@@ -36,10 +35,10 @@ class Snake():
             self.position[0] += speed
         if self.direction == "West":
             self.position[0] -= speed
-        self.head.insert(0, list(self.position))
-        if pygame.sprite.spritecollideany(snake, AppleSprite):
-        #self.position == applePos:
+        self.head.insert(0,list(self.position))
+        if self.position[0] < int(Apple.position[0]) + 10 and self.position[0] > int(Apple.position[0]) -10 and self.position[1] < int(Apple.position[1]) + 10 and self.position[1] > int(Apple.position[1]) - 10:
             return 1 
+
         else:
             self.head.pop()
             return 0
@@ -62,36 +61,42 @@ class Snake():
     def getBody(self):
         return self.head
 
-class Apple(pygame.sprite.Sprite):
+class Apple():
     def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.transform.scale(pygame.image.load("C:\Users\Patri\OneDrive\Desktop\Computer Science\Snake\Bruh.png"),(40,40))
-        self.image_init = self.image
-        self.rect = self.image.get_rect()
-        self.rect.centerx = random.randint(50,450)
-        self.rect.centery=random.randint(50,450)
-        self.position = (self.rect.centerx,self.rect.centery)
+        #pygame.sprite.Sprite.__init__(self)
+        #self.image = pygame.transform.scale(pygame.image.load("Bruh.png"),(20,20))
+        #self.x = random.randint(50,450)
+        #self.y = random.randint(50,450)
+        #self.position = [self.x,self.y]
+        #self.image_init = self.image
+        #self.rect = self.image.get_rect()
+        #self.rect.centerx = random.randint(50,450)
+        #self.rect.centery=random.randint(50,450)
+        #self.position = (self.rect.centerx,self.rect.centery)
+        self.position = [random.randint(50,450), random.randint(50,450)]
         self.AppleOnScreen = True
 
     def spawnApple(self):
         if self.AppleOnScreen == False:
-            (self.rect.centerx,self.rect.centery) = [random.randint(10,490), random.randint(10,490)]
+            self.position = [random.randint(10,490), random.randint(10,490)]
             self.AppleOnScreen = True
-        return (self.rect.centerx,self.rect.centery)
+        return self.position
 
-    def setAppleOnScreen(self,b):  
-        self.AppleOnScreen = b
-
+    def setAppleOnScreen(self):  
+        self.AppleOnScreen = False
+    
+#all_sprites.add(Apple)
 res = (500,500)
 screen = pygame.display.set_mode(res)
 snake = Snake()
 Apple = Apple()
+fps = pygame.time.Clock()
 
 def Lose():
     pygame.quit()
     sys.exit()
 
-AppleSprite.add(Apple)
+
 #all_sprites_list.add(Apple)
 
 while True: 
@@ -101,6 +106,8 @@ while True:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 Lose()
+    
+    #print(Apple.position, snake.position)
     
     keysPressed=pygame.key.get_pressed()
     
@@ -112,22 +119,25 @@ while True:
         snake.direction="East"
     if keysPressed[ord('a')]:
         snake.direction="West"
+    #if keysPressed[ord('e')]:
+        #print(Apple.position, snake.position)
 
-    for pos in snake.getBody():
-        pygame.draw.rect(screen, pygame.Color(0,255,0),pygame.Rect(pos[0], pos[1], 30, 30))
-    
-    if(snake.move(Apple.SpawnApple)==1):
+    ApplePos = Apple.spawnApple()
+    if(snake.move(ApplePos)==1):
         score += 1
-        Apple.setAppleOnScreen(False)
+        Apple.setAppleOnScreen()
+        print(Apple.AppleOnScreen)
 
     screen.fill(pygame.Color(255,255,255))
 
-    AppleSprite.update()
-    AppleSprite.draw(screen)
-    pygame.display.flip()
+    for pos in snake.getBody():
+        pygame.draw.rect(screen, pygame.Color(0,255,0),pygame.Rect(pos[0], pos[1], 10, 10))
+    
+    pygame.draw.rect(screen, pygame.Color(255,0,0), pygame.Rect(ApplePos[0],ApplePos[1],10,10))
 
-    if (snake.Death()==1):#
+
+    if (snake.Death()==1):
         Lose()
     pygame.display.set_caption("Snake. Score: " + str(score))
     pygame.display.update()
-    clock.tick(30)
+    fps.tick(24)
